dc65e7bebe56df33049d680bc0b1a9413abb1347
Dan Ruscoe <danruscoe@gmail.com>
2014-10-30 12:44:24 -0700

    Basic plugin code.

--- plugins/entity/plugin.js
+++ plugins/entity/plugin.js
@@ -4,4 +4,24 @@
 
 CKEDITOR.plugins.add('entity', {
 
+  // Register the icon.
+  icons: 'entity',
+
+  // Initialize the plugin.
+  init: function( editor ) {
+
+    // Define the editor command to open a dialog window.
+    editor.addCommand('entity', new CKEDITOR.dialogCommand('entityDialog'));
+
+    // Create a toolbar button for the dialog window.
+    editor.ui.addButton('Entity', {
+      label: 'Insert Entity',
+      command: 'entity',
+      toolbar: 'insert'
+    });
+
+    // Register the file containing the dialog window code.
+    CKEDITOR.dialog.add('entityDialog', this.path + 'dialogs/entity.js');
+  }
+
 });

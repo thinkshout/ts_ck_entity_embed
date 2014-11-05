d687e61e8eb9eaeace94d0e59f87ca27428bc7ad
Dan Ruscoe <danruscoe@gmail.com>
2014-11-05 14:53:21 -0800

    Entity search results now displayed in CKEditor plugin.

--- plugins/entity/dialogs/entity.js
+++ plugins/entity/dialogs/entity.js
@@ -20,9 +20,14 @@ CKEDITOR.dialog.add('entityDialog', function(editor) {
         elements: [
           {
             type: 'text',
-            id: 'entity-' + entity_type + '-id',
+            id: 'entity-' + entity_type + '-search',
             label: 'Find ' + entity_type,
-          }
+          },
+          {
+            type: 'html',
+            id: 'entity-' + entity_type + '-results',
+            html: '<div id="entity-' + entity_type + '-results">No results</div>',
+          },
         ]
       };
 
@@ -62,13 +67,49 @@ CKEDITOR.dialog.add('entityDialog', function(editor) {
 var TSCKEntityEmbedEntityDialog = {};
 
 jQuery(document).ready(function ($) {
+
   TSCKEntityEmbedEntityDialog.init = function () {
 
     $(".cke_dialog_contents").find(".cke_dialog_page_contents").each(function (i) {
 
-      console.log($(this).attr("name"));
+      var entity_type = $(this).attr("name").replace("tab-", "");
+
+      console.log(entity_type);
+
+      $(this).find('.cke_dialog_ui_input_text').change(function () {
+
+        $.get('/admin/ts_ck_entity_embed/entities/' + entity_type + '/' + $(this).val(), function (data) {
+
+          TSCKEntityEmbedEntityDialog.populateResults(entity_type, data);
+
+        });
+
+      });
 
     });
 
+  },
+
+  TSCKEntityEmbedEntityDialog.populateResults = function(entity_type, data) {
+
+    var resultsElement = $('#entity-' + entity_type + '-results');
+
+    console.log(resultsElement);
+
+    console.log("results: " + data.length);
+
+    if (data.length === 0) {
+      resultsElement.html('No results');
+    } else {
+      resultsElement.html('<ul id="entity-' + entity_type + '-results-list"></ul>');
+
+      for (var i = 0; i < data.length; i++) {
+        $('#entity-' + entity_type + '-results-list').append(
+          '<a id="' + entity_type + '-' + data[0].id + '" href="#">' + data[0].title + '</a>'
+        );
+      }
+    }
+
   }
+
 });

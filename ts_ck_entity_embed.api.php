7de855faf2db9013e55cb96490c2729c89556a32
Dan Ruscoe <danruscoe@gmail.com>
2014-12-18 13:23:15 -0800

    Added view_id property to entity info.

--- ts_ck_entity_embed.api.php
+++ ts_ck_entity_embed.api.php
@@ -8,6 +8,9 @@
 /**
  * Provides an array of embeddable entity information.
  *
+ * Entities can be searched and selected using either a Drupal view
+ * or a field in the entity database table.
+ *
  * @return array
  *   Array of entity information indexed by entity type.
  */
@@ -16,8 +19,12 @@ function hook_ts_ck_entity_embed_entity_info() {
     'bean' => array(
       // The method used to render the entity when embedded.
       'render_method' => 'ts_ck_entity_embed_render_bean',
-      // The database table field used to search for this type of entity.
+      // The ID of the Drupal view used to search for and select entities.
+      // If empty or missing, the 'search_field' property will be used.
+      'view_id' => 'fh_ck_entity_embed_bean',
+      // The database table field used to search for entities.
       // Usually whatever contains the entity name / title.
+      // Ignored if 'view_id' is not empty.
       'search_field' => 'label',
     ),
   );

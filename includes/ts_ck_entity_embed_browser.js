e4e448fcd70b9206e6513611ecfa33ac27c1bfd2
Dan Ruscoe <danruscoe@gmail.com>
2014-12-29 14:45:16 -0800

    Fixed entity highlighting bug,

--- includes/ts_ck_entity_embed_browser.js
+++ includes/ts_ck_entity_embed_browser.js
@@ -17,12 +17,13 @@ function TSCKEntityEmbedEntityBrowser (jQuery, browser_element) {
   this.handleEntitySelect = function (event) {
 
     // Reset selected entity highlighting.
-    this.$("div.entity").removeClass("selected-entity");
+    this.browser_element.contents().find("div.entity").removeClass("selected-entity");
 
     var path = this.$(event.currentTarget).attr("about");
     console.log('Clicked entity: ' + path);
 
-    this.$(event.target).addClass("selected-entity");
+    // Add highlighting to selected entity.
+    this.$(event.currentTarget).addClass("selected-entity");
 
     // TODO: Remove hard-coded entity type.
     TSCKEntityEmbedEntityDialog.selectBrowserEntity('bean', path);
